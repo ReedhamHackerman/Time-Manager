9dd355c14118b4068d1315c23aa8b9684906bd27
ReedhamHackerman <48519104+ReedhamHackerman@users.noreply.github.com>
2020-12-21 21:32:57 -0500

    First Commit
    Helper Class For Invoke method Better Than Invoke

---
+++ Assets/Resources/Scripts/MainScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public enum actions
+{
+    hello,no , hi ,ohk,bye
+}
+public class MainScript : MonoBehaviour
+{
+    public actions a;
+    
+
+   
+   // string s = "BOo";
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TimeManager.Instance.Initialize();
+        //TimeManager.Instance.AddDelegate(MyString,5.5f);
+        string g = "Hellloooooo";
+        TimeManager.Instance.Initialize();   
+        TimeManager.MyDelegate d = () => { OutputString(g); };
+        TimeManager.Instance.AddDelegate(d, 4.5f,3);
+        g = "No Way";
+        TimeManager.MyDelegate m = () => { OutputString(g); };
+        TimeManager.Instance.AddDelegate(m, 9f,3);
+
+        //TimeManager.Instance.AddArgumentDeleGate(MyStr, 5.5f);
+        // TimeManager.Instance.AddArgumentDeleGate(
+
+
+      
+
+
+        int[] arr = new int[] { 1, 2, 3, 4, 5 };
+        IEnumerable<string> lol = arr.Select(x => x.ToString());
+        for (int i = 0; i < lol.Count(); i++)
+        {
+            Debug.Log("Element at : " + arr.ElementAt(i));
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeManager.Instance.Refresh();
+    }
+    public void MyString()
+    {
+        Debug.Log("Hello");
+    }
+
+    public string MyStr(string b)
+    {
+        return b;
+    }
+
+    public void MyString(string s,int a ,float j)
+    {
+        Debug.Log("Its String: " + s + " Its Int: " + a + " float j: " + j);
+    }
+
+    public void OutputString(string s)
+    {
+        Debug.Log("String is: " + s);
+    }
+}
---
+++ Assets/Resources/Scripts/TimeManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager
+{
+    public delegate void MyDelegate();
+    MyDelegate myDelegate;
+    private static TimeManager instance;
+    public static TimeManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new TimeManager();
+            }
+            return instance;
+        }
+    }
+    public delegate string MyArgumentDelegate(string S);
+
+   
+
+
+    List<DelegateTimer> delegateTimerList;
+    public void Initialize()
+    {
+        delegateTimerList = new List<DelegateTimer>();
+    }
+    public void Refresh()
+    {
+        for (int i = delegateTimerList.Count-1; i >= 0; i--)
+        {
+            if (delegateTimerList[i].timeToInvoke<=Time.time)
+            {
+                for (int j = 0; j < delegateTimerList[i].repeat; j++)
+                {
+                    try
+                    {
+                        delegateTimerList[i].delegateToInvoke();
+                    }
+                    catch (System.Exception)
+                    {
+
+                        Debug.LogError("Exception Thrown");
+                    }
+                   
+                }
+                
+                delegateTimerList.RemoveAt(i);
+            }
+        }
+    }
+    public void EndGame()
+    {
+        delegateTimerList.Clear();
+    }
+
+
+    public void AddDelegate(MyDelegate del,float time,int repeat)
+    {
+        DelegateTimer toADD = new DelegateTimer(Time.time + time, del,repeat);
+        delegateTimerList.Add(toADD);
+       
+    }
+
+    public void AddArgumentDeleGate(MyArgumentDelegate arg,float time)
+    {
+        DelegateTimer toadd = new DelegateTimer(time + Time.time, arg);
+        delegateTimerList.Add(toadd);
+    }
+
+
+
+    private class DelegateTimer
+    {
+       public float timeToInvoke;
+       public   MyDelegate delegateToInvoke;
+       public MyArgumentDelegate MyArgumentDelegate;
+       public int repeat;
+        public DelegateTimer(float timeOfInvo,MyDelegate del)
+        {
+            this.timeToInvoke = timeOfInvo;
+            this.delegateToInvoke = del;
+
+        }
+        public DelegateTimer(float timeOfInvo, MyArgumentDelegate myArgumentDelegate)
+        {
+            this.timeToInvoke = timeOfInvo;
+            this.MyArgumentDelegate = myArgumentDelegate;
+        }
+        public DelegateTimer(float timeOfInvo,MyDelegate del,int repeat)
+        {
+            this.timeToInvoke = timeOfInvo;
+            this.delegateToInvoke = del;
+            this.repeat = repeat;
+        }
+
+    }
+}
